ce18750e2832413393a48b6f87a51e7ffa803144
Richard Seal <richyseal@gmail.com>
2024-09-13 16:56:42 -0400

    tmp

--- headapps/nextjs-starter/src/lib/graphql-editing-service.ts
+++ headapps/nextjs-starter/src/lib/graphql-editing-service.ts
@@ -3,7 +3,7 @@ import clientFactory from 'lib/graphql-client-factory';
 
 /**
  * GraphQL Editing Service instance. Used to fetch editing data in Pages preview (editing) Metadata Edit Mode.
- */
+ **/
 export const graphQLEditingService = new GraphQLEditingService({
   clientFactory,
 });
